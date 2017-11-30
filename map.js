73cab107f511a78a5d3e3ea963211ac9f659f619
Frank Lam <frank.is.missing@gmail.com>
2017-11-30 20:55:15 +0000

    Initial commit

---
+++ map.js
@@ -0,0 +1,14 @@
+var words = ["ground", "control", "to", "major", "tom"];
+
+map(words, function(word) {
+  return word.length;
+});
+
+map(words, function(word) {
+  return word.toUpperCase();
+});
+
+map(words, function(word) {
+  return word.split('').reverse().join('');
+});
+
